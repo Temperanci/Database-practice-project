b2da5bdd3047af492e015047b1fde3f7b21a9c7e
Temperanci <temperance131@gmail.com>
2021-05-27 14:07:52 +0800

    5.27

--- Arknights/Fore_end/src/admin/page/Upload_record.vue
+++ Arknights/Fore_end/src/admin/page/Upload_record.vue
@@ -2,56 +2,24 @@
     <div>
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-setting"></i> 管理</el-breadcrumb-item>
-                <el-breadcrumb-item>收藏列表</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-setting"></i> 作战汇报</el-breadcrumb-item>
+
             </el-breadcrumb>
         </div>
-        <div>
-            <el-table :data="data" border style="width: 90%" ref="multipleTable" >
-                <el-table-column label="ID" prop="id" width="80px" ></el-table-column>
-                <el-table-column label="网站名称" prop="wname" width="100px" ></el-table-column>
-                <el-table-column label="网站地址" prop="wurl" width="300px" ></el-table-column>
-                <el-table-column label="权限" prop="type" width="80px" ></el-table-column>
-                <el-table-column label="点击次数" prop="count" width="70px" ></el-table-column>
-                <el-table-column label="创建时间" prop="ctime" width="250px" ></el-table-column>
-                <el-table-column label="操作" width="320px" >
-                    <template slot-scope="scope" width="100px">
-                        <el-button type="text" @click="gotourl(scope.row)" >进入</el-button>
-                        <el-button type="text" @click="openedit(scope.row)" >修改</el-button>
-                        <el-button type="text" @click="del_fav(scope.row)" >删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table><el-button type="primary" @click="addfavorite()" align="center">添加新收藏</el-button>
+
+        <div>  <el-checkbox-group
+            v-model="checkedCities"
+            :min="1"
+            :max="8">
+            <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+             </el-checkbox-group>
         </div>
-        <el-dialog
-            width="30%"
-            title="修改"
-            :visible.sync="dialogFormVisibleed">
-            <div>
-                <el-form :model="form" :rules="rules" ref="form" label-width="150px">
-                    <el-form-item label="网站名称" prop="wname">
-                        <el-input v-model="form.wname" placeholder="请输入网站名称"></el-input>
-                    </el-form-item>
-                    <el-form-item label="网站地址" prop="password2">
-                        <el-input v-model="form.wurl" placeholder="请输入网站地址"></el-input>
-                    </el-form-item>
-                    <el-form-item label="权限" prop="type">
-                        <el-select v-model="form.type" placeholder="请设置权限">
-                            <el-option
-                                v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item style="text-align: center" >
-                        <el-button @click="dialogFormVisibleed = false" >取消</el-button>
-                        <el-button type="primary" @click="submit('form')">修改</el-button>
-                    </el-form-item>
-                </el-form>
-            </div>
-        </el-dialog>
+
+        <div>
+           <el-button type="primary" @click="addfavorite()" align="center">添加新记录</el-button>
+        </div>
+
+
         <el-dialog
             width="30%"
             title="添加收藏"
@@ -86,7 +54,9 @@
 
 <script>
     import main from "../../main";
+
     export default {
+
         data: function(){
             return {
                 data:[],
@@ -136,53 +106,8 @@
                     }
                 );
             },
-            gotourl(row){ //进入指定的网站
-                this.$http.post(main.url+"/favorite/count",
-                    {'id': row.id},
-                    {
-                        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-                        emulateJSON: true
-                    }).then(
-                    success=> {
-                        window.open(row.wurl, "_blank");
-                        this.init();
-                    }
-                )
-            },
-            openedit(row){ //修改收藏内容，按钮按下后
-                this.dialogFormVisibleed=true;
-                this.form={
-                    id: row.id,
-                    wname: row.wname,
-                    wurl: row.wurl,
-                    type: row.type==="公开"?0:1
-                };
-            },
-            submit(form){ //修改收藏内容，提交
-                this.$http.post(main.url+"/favorite/update",
-                    {
-                        'id': this.form.id,
-                        'wname': this.form.wname,
-                        'wurl': this.form.wurl,
-                        'type': this.form.type
-                    },
-                    {
-                        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-                        emulateJSON: true
-                    }).then(
-                    success=> {
-                        this.$message({type: 'success', message: '修改成功'});
-                        this.form={
-                            id: '',
-                            wname:'',
-                            wurl:'',
-                            type:''
-                        };
-                        this.dialogFormVisibleed = false;
-                        this.init();
-                    }
-                )
-            },
+
+
             addfavorite(){this.dialogFormVisibleed1=true},
             addnew(form){ //添加新收藏
                 if(this.form.wname==="")
@@ -217,27 +142,7 @@
                     this.dialogFormVisibleed1 = false;
                 }
             },
-            del_fav(row){ //删除收藏记录
-                this.$confirm('请确认是否要删除该收藏记录！', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.$http.post(main.url+"/favorite/del",
-                        {'id': row.id,},
-                        {
-                            headers: {'Content-Type':'application/x-www-form-urlencoded'},
-                            emulateJSON: true
-                        }).then(
-                        success=> {
-                            this.$message({type: 'success', message: '已删除'});
-                            this.init();
-                        }
-                    );
-                }).catch(() => {
-                    this.$message({type: 'info', message: '已取消'});
-                });
-            },
+
         }
     }
 </script>
