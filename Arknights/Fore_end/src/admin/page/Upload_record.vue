9b6250b0cf9802a02ecbe85a0964c83c4e301d79
Quinn-SU <67184811+Quinn-SU@users.noreply.github.com>
2021-05-28 19:25:37 +0800

    实现了上传用户作战记录

--- Arknights/Fore_end/src/admin/page/Upload_record.vue
+++ Arknights/Fore_end/src/admin/page/Upload_record.vue
@@ -6,78 +6,61 @@
 
             </el-breadcrumb>
         </div>
+        <div class="form-box">
+            <el-form :model="form" :rules="rules" ref="form" label-width="150px">
+                <el-form-item label="选择地图" prop="type">
+                    <el-select v-model="form.type" placeholder="请选择地图">
+                         <el-option
+                             v-for="item in maps"
+                             :key="item.value"
+                             :label="item.label"
+                                :value="item.value">
+                         </el-option>
+                    </el-select>
+                </el-form-item>
 
-        <div><el-checkbox-group v-model="checkList">
-            <el-checkbox label="复选框 A"></el-checkbox>
-            <el-checkbox label="复选框 B"></el-checkbox>
-            <el-checkbox label="复选框 C"></el-checkbox>
-            <el-checkbox label="禁用" disabled></el-checkbox>
-            <el-checkbox label="选中且禁用" disabled></el-checkbox>
-        </el-checkbox-group></div>
+
+            </el-form>
+        </div>
 
         <div>
-           <el-button type="primary" @click="addfavorite()" align="center">添加新记录</el-button>
+
+            <el-checkbox-group v-model="checkboxGroup1">
+                <el-checkbox-button v-for="item in items" :label="item" :key="item" >{{item}}</el-checkbox-button>
+            </el-checkbox-group>
+
         </div>
 
 
-        <el-dialog
-            width="30%"
-            title="添加收藏"
-            :visible.sync="dialogFormVisibleed1">
-            <div class="form-box">
-                <el-form :model="form" :rules="rules" ref="form" label-width="150px">
-                    <el-form-item label="网站名称" prop="wname">
-                        <el-input v-model="form.wname" placeholder="请输入网站名称"></el-input>
-                    </el-form-item>
-                    <el-form-item label="网站地址" prop="wurl">
-                        <el-input v-model="form.wurl" placeholder="请输入网站地址"></el-input>
-                    </el-form-item>
-                    <el-form-item label="权限" prop="type">
-                        <el-select v-model="form.type" placeholder="请设置权限">
-                            <el-option
-                                v-for="item in options"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item style="text-align: center" >
-                        <el-button @click="dialogFormVisibleed1 = false" >取消</el-button>
-                        <el-button type="primary" @click="addnew(form)">添加</el-button>
-                    </el-form-item>
-                </el-form>
-            </div>
-        </el-dialog>
+        <div style="margin-top: 20px">
+           <el-button type="primary" @click="addnew()" align="center">添加新记录</el-button>
+        </div>
+
+
+
     </div>
 </template>
 
 <script>
     import main from "../../main";
+    const itemOptions = [ '源岩', '固源岩', '双酮', '酯原料','代糖','异铁碎片',
+        '破损装置', '赤金','初级作战记录','聚酸酯', '碳', '技巧概要·卷1','装置'];
     export default {
-        data: function(){
-            return {
-                data:[],
-                options:[{value: 0, label: "公开"},{value: 1, label: "私有"}],
-                dialogFormVisibleed:false,
-                dialogFormVisibleed1:false,
+    data: function(){
+        return {
+            data:[],
+            maps:[{value: 1, label: "1-1 孤岛"},{value: 2, label: "1-2 蔓延"},{value: 3, label: "1-3 狂奔"},
+                {value: 4, label: "1-4 先兆"},{value: 5, label: "1-5 麻痹"},{value: 6, label: "1-6 灾难"},
+                {value: 7, label: "1-7 暴君"},{value: 8, label: "1-8 意志"},{value: 9, label: "1-9 溃散"}
+            ],
+            items: itemOptions,
+
+            checkboxGroup1: [],
                 form:{
-                    id: '',
-                    wname:'',
-                    wurl:'',
+
                     type:''
-                },
-                rules:{
-                    wname:[
-                        {required:true,message:'请输入网站名称',trigger:'blur'}
-                    ],
-                    wurl:[
-                        {required:true,message:'请输入网站地址',trigger:'blur'}
-                    ],
-                    type:[
-                        {required:true,message:'请设置权限',trigger:'blur'}
-                    ]
                 }
+
             }
         },
         created(){
@@ -87,51 +70,56 @@
         },
         methods:{
             init(){
-                this.$http.post(main.url+"/favorite/list",
-                    {'uid': localStorage.getItem('id')},
-                    {
-                        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-                        emulateJSON: true
-                    }).then(
-                    success=>{
-                        this.data=success.data;
-                        for(let i=0;i<this.data.length;i++){
-                            if(this.data[i].type===0)
-                                this.data[i].type = "公开";
-                            else
-                                this.data[i].type = "私有";
-                        }
-                    }
-                );
+
             },
 
 
-            addfavorite(){this.dialogFormVisibleed1=true},
-            addnew(form){ //添加新收藏
-                if(this.form.wname==="")
-                    this.$message({type: 'error', message: '网站名称！'});
-                else if(this.form.wurl==="")
-                    this.$message({type: 'error', message: '网站地址不能为空！'});
-                else if(this.form.type==="")
-                    this.$message({type: 'error', message: '请设置权限！'});
+
+            addnew(form,checkboxGroup1){ //添加新纪录
+
+
+                alert(this.form.type);
+                var isChecked=new Array(13)//定义数组存储checkbox是否选中
+                for(var a=0;a<13;a++)
+                    isChecked[a]=0;
+
+                for(var a=0;a<this.checkboxGroup1.length;a++)//如果选中，即checkbox里能找到，把数组值设为1
+                {
+                    isChecked[itemOptions.indexOf(this.checkboxGroup1[a])]=1;
+
+                }
+
+                if(this.form.type==="")
+                    this.$message({type: 'error', message: '请选择地图！'});
+
+
                 else{
-                    this.$http.post(main.url+"/favorite/add",
+                    this.$http.post(main.url+"/user_map/add",
                         {
-                            'uid': localStorage.getItem('id'),
-                            'wname': this.form.wname,
-                            'wurl': this.form.wurl,
-                            'type': this.form.type
+                            'user_id': localStorage.getItem('id'),
+                            'map_num': this.form.type,
+                            'item1': isChecked[0],
+                            'item2': isChecked[1],
+                            'item3': isChecked[2],
+                            'item4': isChecked[3],
+                            'item5': isChecked[4],
+                            'item6': isChecked[5],
+                            'item7': isChecked[6],
+                            'item8': isChecked[7],
+                            'item9': isChecked[8],
+                            'item10': isChecked[9],
+                            'item11': isChecked[10],
+                            'item12': isChecked[11],
+                            'item13': isChecked[12]
                         },
                         {
                             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
                             emulateJSON: true
                         }).then(
                         success => {
-                            this.$message({type: 'success', message: '添加成功'});
+                            this.$message({type: 'success', message: '上传作战记录成功'});
                             this.form = {
-                                id: '',
-                                wname:'',
-                                wurl:'',
+
                                 type:''
                             };
                             this.init();
